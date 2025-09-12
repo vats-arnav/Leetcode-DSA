54a7bebc17cf2544e7adeeeb1a34bd4b887a734c
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2025-09-13 00:00:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (12.67%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            if (n & 1) {
+                sum++;
+            }
+            n = n >> 1;
+        }
+        return sum;
+    }
+};
