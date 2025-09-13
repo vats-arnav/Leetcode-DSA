ecc8662472501afb54e44dfa8e2a32510b81e315
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2025-09-13 13:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (80.80%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> dup;
-        unordered_set<int> cnt;
-
-        for (int num : nums) {
-            if (cnt.count(num)) {
-                dup.push_back(num);
-            } else {
-                cnt.insert(num);
+        for(int num : nums){
+            num = abs(num);
+            if(nums[num-1]<0){
+                dup.push_back(abs(num));
             }
+            nums[num-1] = nums[num-1]*-1;
         }
         return dup;
+
     }
 };
