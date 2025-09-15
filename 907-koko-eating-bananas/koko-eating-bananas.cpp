6d2e4977e8b56776f49ef1dde402c78cadf9491d
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2025-09-15 23:45:50 +0530

    Time: 11 ms (39.53%) | Memory: 22.8 MB (96.31%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -4,12 +4,14 @@ public:
         long long sum = 0;
         long long start = 1;
         long long end = *max_element(piles.begin(), piles.end());
-        long long mid = 0;
-        int res = 0;
+        int res = end;
         while (start <= end) {
-            mid = start + (end - start) / 2;
-            cout << mid << endl;
-            if (time(piles, mid) <= h) {
+            long long mid = start + (end - start) / 2;
+            long long total = 0;
+            for (int bananas : piles) {
+                total += (bananas + mid - 1) / mid; 
+            }
+            if (total <= h) {
                 end = mid - 1;
                 res = mid;
             } else {
@@ -17,15 +19,5 @@ public:
             }
         }
         return res;
-    }
-
-    long long int time(vector<int>& piles, int t) {
-        long long int total_hrs = 0;
-        for (long long i = 0; i < piles.size(); i++) {
-            long long time = ceil((double)piles[i] / t);
-            total_hrs += time;
-        }
-        // cout<<total_hrs<<endl;
-        return total_hrs;
     }
 };
