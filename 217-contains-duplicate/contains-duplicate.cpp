db28bcc4db793961ac1c430f5a40ed39935a4cbf
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2025-09-20 22:52:23 +0530

    Time: 48 ms (64.43%) | Memory: 90.7 MB (60.58%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> dup;
+        unordered_set<int> dup;
         for(int n:nums){
             if(dup.count(n)) return true;
-            dup[n] =1;
+            dup.insert(n);
         }
         return false;
     }
