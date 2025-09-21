a9e358b179298a17b7954f87b66ae2d04ea70dc0
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2025-09-21 10:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.11%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int leftp = 0;
+        int rightp = 0;
+        int n = nums.size();
+        while (rightp < n) {
+            if (nums[rightp] != 0) {
+                nums[leftp] = nums[rightp];
+                leftp++;
+            }
+            rightp++;
+        }
+        while (leftp < n) {
+            nums[leftp] = 0;
+            leftp++;
+        }
+    }
+};
