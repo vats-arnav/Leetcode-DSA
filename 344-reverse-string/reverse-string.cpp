bd8d3e74c7233e7974263524e3c374d530ff538b
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2025-12-08 21:56:48 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (97.41%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int p1 = 0;
+        int p2 = s.size() - 1;
+
+        while (p1 < p2) {
+            char temp = s[p1];
+            s[p1] = s[p2];
+            s[p2] = temp;
+
+            p1++;
+            p2--;
+        }
+    }
+};
