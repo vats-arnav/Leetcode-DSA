f680f3f5f29645ed4f41690e396cecb3142fb721
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-01 15:07:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,28 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         return s==t;
+//     }
+// };
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        int n = s.length();
+        if(n!=t.length())
+            return false;
+        int arr[26] ={0};
+        for (int i =0;i<n;i++) {
+            arr[s[i] - 'a']++;
+            arr[t[i] -'a']--;
+        }
+        for (int j=0;j<26;j++) {
+            if(arr[j]!=0)
+                return false;
+        }
 
-        return s==t;
+        return true;
     }
 };
