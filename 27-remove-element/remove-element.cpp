38af36947d807858bd81abc96cb252ea4345b6f3
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-01 15:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (82.25%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,17 +4,15 @@ public:
         int n = nums.size();
         int ptr1 = 0;
         int ptr2 = 0;
-        int count = 0;
         if (nums.size() == 0)
             return 0;
         while (ptr2 < n) {
             if (nums[ptr2] != val) {
-                count++;
                 nums[ptr1] = nums[ptr2];
                 ptr1++;
             }
             ptr2++;
         }
-        return count;
+        return ptr1;
     }
 };
