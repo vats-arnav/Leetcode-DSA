b14eac5fac554589faadf21e451eb8a5eec69aaf
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-02 10:30:10 +0530

    Time: 2 ms (18.22%) | Memory: 12.9 MB (29.98%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int calPoints(vector<string>& operations) {
         stack<int> st;
-
+        int sum =0;
         for (const string& s : operations) {
 
             if (s == "+") {
@@ -10,24 +10,22 @@ public:
                 int b = st.top();
                 st.push(a); 
                 st.push(a + b);
+                sum += a;
+                sum+=b;
 
             } else if (s == "D") {
                 st.push(2 * st.top());
+                sum+=st.top();
 
             } else if (s == "C") {
+                sum-=st.top();
                 st.pop();
 
             } else {
                 st.push(stoi(s));
+                sum+=st.top();
             }
         }
-
-        int sum = 0;
-        while (!st.empty()) {
-            sum += st.top();
-            st.pop();
-        }
-
         return sum;
     }
 };
