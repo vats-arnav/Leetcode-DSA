6af1aac34cccf053e311592759b2f0d8c426abbd
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-04 21:17:40 +0530

    Time: 2 ms (78.08%) | Memory: 7.7 MB (87.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,27 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n==0){
+//             return 0;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+//         return fib(n-1) + fib(n-2);
+//     }
+// };
+
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
-            return 0;
+        int prev1 = 0, prev2 = 1, curr;
+        if (n <= 0)
+            return n;
+        for (int i = 2; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
         }
-        if(n==1){
-            return 1;
-        }
-        return fib(n-1) + fib(n-2);
+        return curr;
     }
 };
