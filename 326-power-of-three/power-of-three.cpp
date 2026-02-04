1a6a2ee31e5a65b7aa8ee6a6f0a72dc6ea31d66e
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-04 21:04:23 +0530

    Time: 3 ms (58.70%) | Memory: 8.9 MB (41.67%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,15 +1,15 @@
-// class Solution {
-// public:
-//     bool isPowerOfThree(int n) {
-//         if(n<=0) return false;
-//         else if(n==1) return true;
-//         return n%3==0 && isPowerOfThree(n/3);;
-//     }
-// };
-
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return n > 0 && 1162261467 % n == 0;
+        if(n<=0) return false;
+        else if(n==1) return true;
+        return n%3==0 && isPowerOfThree(n/3);;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         return n > 0 && 1162261467 % n == 0;
+//     }
+// };
