0c6c068e13a43262684535ce2179cedacbf6ce6e
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-06 18:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (50.73%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,49 @@
+// class Solution {
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> ds;
+//         vector<int> freq(nums.size(), 0);
+//         recurPermute(ds,nums,ans,freq);
+//         return ans;
+//     }
+
+//     void recurPermute(vector<int> &ds,vector<int>&nums,vector<vector<int>> &ans,vector<int>&freq){
+//         if(ds.size()==nums.size()){
+//             ans.push_back(ds);
+//             return;
+//         }
+//         for(int i=0;i<nums.size();i++){
+//             if(!freq[i]){
+//                 ds.push_back(nums[i]);
+//                 freq[i] =1;
+//                 recurPermute(ds,nums,ans,freq);
+//                 freq[i]=0;
+//                 ds.pop_back();
+//             }
+//         }
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        vector<int> freq(nums.size(), 0);
-        recurPermute(ds,nums,ans,freq);
+        recurPermute(0,nums,ans);
         return ans;
     }
 
-    void recurPermute(vector<int> &ds,vector<int>&nums,vector<vector<int>> &ans,vector<int>&freq){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+    void recurPermute(int index,vector<int>&nums,vector<vector<int>>&ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i] =1;
-                recurPermute(ds,nums,ans,freq);
-                freq[i]=0;
-                ds.pop_back();
-            }
+
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            recurPermute(index+1,nums,ans);
+            swap(nums[index],nums[i]);
         }
     }
 };
