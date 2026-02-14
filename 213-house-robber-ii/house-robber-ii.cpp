385a4c612f9dd5e21aef845950dcd39c3ccdff5b
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-15 00:06:34 +0530

    Time: 2 ms (9.50%) | Memory: 10.8 MB (38.48%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -13,7 +13,6 @@ public:
         }
         dp[1]= max(nums[0], nums[1]);
         dp2[1] = nums[1];
-        dp2[2]= max(nums[2], nums[1]);
         for(int i=2;i<n-1;i++){
             dp[i] = max(dp[i-2]+nums[i],dp[i-1]);
         }
