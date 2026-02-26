7caae236ec2a589a4597b832304c26c801ee0ffc
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-26 23:27:47 +0530

    Time: 7 ms (88.02%) | Memory: 11.2 MB (95.31%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int x = f(word1, word2);
+        int z = min(word1.size(),word2.size()) -x;
+        return max(word1.size(),word2.size()) - x+z;
+    }
+
+    int f(const string& text1, const string& text2) {
+        int n1 = text1.size();
+        int n2 = text2.size();
+        vector<int> prev(text2.size() + 1,0),curr(text2.size() + 1,0);
+        for (int i = 0; i <= n2; i++) {
+            prev[i] =0;
+        }
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (text1[i - 1] == text2[j- 1]) {
+                    curr[j] = 1 + prev[j - 1];
+                } else {
+                    curr[j] = max(prev[j], curr[j - 1]);
+                }
+            }
+            prev = curr;
+        }
+
+        return curr[n2];
+    }
+};
