b1f1bc0247fc3fe7da5906c5a306ae5530f24a6e
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-02-28 11:46:23 +0530

    Time: 7 ms (96.63%) | Memory: 8.9 MB (92.29%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,29 +1,67 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        vector<vector<double>> dp(s.size() + 1, vector<double>(t.size() + 1, -1));
-        return f(s, t, dp);
+        return f(s, t);
     }
 
-    int f(const string& text1, const string& text2, vector<vector<double>>& dp) {
+    int f(const string& text1, const string& text2) {
         int n1 = text1.size();
         int n2 = text2.size();
-        for (int i = 1; i <= n2; i++) {
-            dp[0][i] = 0;
-        }
-        for (int i = 0; i <= n1; i++) {
-            dp[i][0] = 1;
-        }
+        vector<double> prev(n2+1,0);
+        prev[0]=1;
         for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
+            for (int j = n2; j>=1; j--) {
                 if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+                    prev[j] = prev[j] + prev[j - 1];
+                } 
+                else {
+                    continue;
                 }
             }
         }
 
-        return (int)dp[n1][n2];
+        return prev[n2];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int numDistinct(string s, string t) {
+//         return f(s, t);
+//     }
+
+//     int f(const string& text1, const string& text2) {
+//         int n1 = text1.size();
+//         int n2 = text2.size();
+//         vector<double> prev(n2+1,0),curr(n2+1,0);
+//         prev[0]=curr[0]=1;
+//         for (int i = 1; i <= n1; i++) {
+//             for (int j = 1; j <= n2; j++) {
+//                 if (text1[i - 1] == text2[j - 1]) {
+//                     curr[j] = prev[j] + prev[j - 1];
+//                 } 
+//                 else {
+//                     curr[j] = prev[j];
+//                 }
+//             }
+//             prev = curr;
+//         }
+
+//         return prev[n2];
+//     }
+// };
