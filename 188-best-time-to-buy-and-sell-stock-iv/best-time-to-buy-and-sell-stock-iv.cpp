d70f7d8faad00d8d91a840302daa151a7ef744cc
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-03-01 21:10:56 +0530

    Time: 3 ms (81.60%) | Memory: 14.4 MB (88.54%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -5,31 +5,79 @@ public:
     int f(int z,vector<int>& prices) {
         int n = prices.size();
         long profit;
-        vector<vector<vector<long>>> dp(n, vector<vector<long>>(2,
-        vector<long>(z+1, 0))); for(int k = 1; k <= z; k++) {
-            dp[0][1][k] = -prices[0];
+        vector<vector<long>> curr(2, vector<long>(z+1,0));
+        vector<vector<long>> prev(2, vector<long>(z+1,0));
+        for(int k = 1; k <= z; k++) {
+            prev[1][k] = -prices[0];
         }
         for (int i = 1; i < n; i++) {
             for (int j = 0; j <= 1; j++) {
                 for (int k = 1; k <= z; k++) {
                     if (j == 0) {
                         profit =
-                            max(dp[i - 1][0][k], +prices[i] + dp[i -
-                            1][1][k]);
+                            max(prev[0][k], +prices[i] + prev[1][k]);
                     }
                     if (j == 1) {
                         profit =
-                            max(dp[i - 1][1][k], -prices[i] + dp[i -
-                            1][0][k-1]);
+                            max(prev[1][k], -prices[i] + prev[0][k-1]);
                     }
 
-                    dp[i][j][k] = profit;
+                    curr[j][k] = profit;
                 }
             }
+            prev = curr;
         }
-        return (int)dp[n - 1][0][z];
+        return (int)prev[0][z];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int maxProfit(int k,vector<int>& prices) { return f(k,prices); }
+
+//     int f(int z,vector<int>& prices) {
+//         int n = prices.size();
+//         long profit;
+//         vector<vector<vector<long>>> dp(n, vector<vector<long>>(2,
+//         vector<long>(z+1, 0))); for(int k = 1; k <= z; k++) {
+//             dp[0][1][k] = -prices[0];
+//         }
+//         for (int i = 1; i < n; i++) {
+//             for (int j = 0; j <= 1; j++) {
+//                 for (int k = 1; k <= z; k++) {
+//                     if (j == 0) {
+//                         profit =
+//                             max(dp[i - 1][0][k], +prices[i] + dp[i -
+//                             1][1][k]);
+//                     }
+//                     if (j == 1) {
+//                         profit =
+//                             max(dp[i - 1][1][k], -prices[i] + dp[i -
+//                             1][0][k-1]);
+//                     }
+
+//                     dp[i][j][k] = profit;
+//                 }
+//             }
+//         }
+//         return (int)dp[n - 1][0][z];
+//     }
+// };
 
 
 
