566e652070238d2b986a936a3b0f7dd999a1bf7d
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-03-01 16:39:24 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (39.73%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -7,20 +7,61 @@ public:
     int f(vector<int>& prices){
         int n = prices.size();
         long profit;
-        vector<vector<long>> dp(n+1,vector<long>(2,-1));
-        dp[n][0] = dp[n][1] =0;
-        for(int i=n-1;i>=0;i--){
+        vector<long> prev(2,-1),curr(2,-1);
+        prev[0]=0;
+        prev[1]= -prices[0];
+        for(int i=1;i<n;i++){
             for(int j=0;j<=1;j++){
                 if(j==0){
-                   profit = max(dp[i+1][0],-prices[i] + dp[i+1][1]);
+                   profit = max(prev[0],+prices[i] + prev[1]);
                 }
                 if(j==1){
-                    profit = max(dp[i+1][1],+prices[i] + dp[i+1][0]);
+                    profit = max(prev[1],-prices[i] + prev[0]);
                 }
 
-                dp[i][j] = profit;
+                prev[j] = profit;
             }
+            //prev = curr;
         }
-        return (int)dp[0][0];
+        return (int)prev[0];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         return f(prices);
+//     }
+
+//     int f(vector<int>& prices){
+//         int n = prices.size();
+//         long profit;
+//         vector<vector<long>> dp(n,vector<long>(2,-1));
+//         dp[0][0]=0;
+//         dp[0][1]= -prices[0];
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<=1;j++){
+//                 if(j==0){
+//                    profit = max(dp[i-1][0],+prices[i] + dp[i-1][1]);
+//                 }
+//                 if(j==1){
+//                     profit = max(dp[i-1][1],-prices[i] + dp[i-1][0]);
+//                 }
+
+//                 dp[i][j] = profit;
+//             }
+//         }
+//         return (int)dp[n-1][0];
+//     }
+// };
