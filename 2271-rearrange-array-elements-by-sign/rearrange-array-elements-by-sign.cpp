b1e570ee5aa1fbf864c7fffd02f47a5ef7b069bb
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-07-15 11:22:25 +0530

    Time: 4 ms (77.46%) | Memory: 128.1 MB (95.48%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        int x=0,y=1;
+        vector<int> ans(nums.size());
+        for(auto i:nums){
+            if(i>0){
+                ans[x] = i;
+                x+=2;
+            }
+            else{
+                ans[y] = i;
+                y+=2;
+            }
+        }
+    return ans;
+    }
+};
+
+
+
+/* class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> arrp;
         vector<int> arrn;
         vector<int> ans;
@@ -20,4 +41,4 @@ public:
 
         return ans;
     }
-};
+};*/
