aa34316462573a5491d3fcf769c87e7d9b5d4d3f
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-07-19 22:11:23 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (85.26%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int cntx = 0, cnty = 0, x, y;
+        vector<int> res;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (cntx == 0 && nums[i] != y) {
+                x = nums[i];
+                cntx = 1;
+            } else if (cnty == 0 && nums[i] != x) {
+                y = nums[i];
+                cnty = 1;
+            } else if (nums[i] == x) {
+                cntx++;
+            } else if (nums[i] == y) {
+                cnty++;
+            } else {
+                cnty--;
+                cntx--;
+            }
+        }
+        cntx = 0;
+        cnty = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == x) {
+                cntx++;
+            }
+            if (nums[i] == y) {
+                cnty++;
+            }
+        }
+        int mini = n/3 + 1;
+        if (cntx >= mini) {
+            res.push_back(x);
+        }
+        if (cnty >= mini && y!=x) {
+            res.push_back(y);
+        }
+
+        return res;
+    }
+};
+
+/*class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int, int> ans;
         vector<int> val;
@@ -17,3 +61,5 @@ public:
         return val;
     }
 };
+
+*/
