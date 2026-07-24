bf53f2c4d48f5a2281e3b84b0854000817366f1d
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-07-24 22:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.42%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    vector<int> val(int numRows) {
+        vector<int> temp;
+        long long ans = 1;
+        temp.push_back(1);
+        for (int i = 1; i < numRows; i++) {
+            ans = ans * (numRows - i);
+            ans = ans / i;
+            temp.push_back(ans);
+        }
+
+        return temp;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for (int i = 1; i <= numRows; i++) {
+            ans.push_back(val(i));
+        }
+        return ans;
+    }
+};
+
+/*
+
+class Solution {
+public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
         ans.push_back({1});
@@ -24,3 +49,4 @@ public:
         return ans;
     }
 };
+*/
