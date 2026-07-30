2e0ff8afa4421096d7aceb9edd2ea06707bd4a9d
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-07-30 23:57:04 +0530

    Time: 4 ms (22.90%) | Memory: 9.4 MB (58.22%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        int arr1[256] = {0};
+        int arr2[256] = {0};
+
+        for (int i = 0; i < s.length(); i++) {
+            if (arr1[s[i]] != arr2[t[i]]) {
+                return false;
+            }
+            arr1[s[i]] = i+1;
+            arr2[t[i]] = i+1;
+        }
+        return true;
+    }
+};
