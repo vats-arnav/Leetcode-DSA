eda5424fbeaffac555a704dd584c846566a87c14
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-07 22:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (82.43%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int myAtoi(string s) {
@@ -29,5 +30,33 @@ public:
         }
 
         return (int)(sign * ans);
+    }
+};
+*/
+class Solution {
+public:
+    int helper(const string& s, int i, long long num, int sign) {
+        if (i >= s.size() || !isdigit(s[i]))
+            return (int)(sign * num);
+
+        num = num * 10 + (s[i] - '0');
+        if (sign * num <= INT_MIN)
+            return INT_MIN;
+        if (sign * num >= INT_MAX)
+            return INT_MAX;
+
+        return helper(s, i + 1, num, sign);
+    }
+    int myAtoi(string s) {
+        int i = 0;
+        while (i < s.size() && s[i] == ' ')
+            i++;
+
+        int sign = 1;
+        if (i < s.size() && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+        return helper(s, i, 0, sign);
     }
 };
