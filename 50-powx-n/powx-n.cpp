f0e16665f605d480c480e6a1663e4ff4e60ffbb1
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-07 22:59:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.53%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -11,7 +11,7 @@ public:
         if (n % 2 == 0) {
             return calc(x * x, n / 2);
         } else {
-            return x * calc(x, n - 1);
+            return x * calc(x*x, n/2);
         }
     }
     double myPow(double x, int n) {
