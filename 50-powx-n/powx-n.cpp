b0ddbd686d98342cabeb2b1cea71119174b8e0a3
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-07 22:30:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.55%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double calc(double x, long long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1 || x == 1) {
+            return x;
+        }
+
+        if (n % 2 == 0) {
+            return calc(x * x, n / 2);
+        } else {
+            return x * calc(x, n - 1);
+        }
+    }
+    double myPow(double x, int n) {
+        long long num = n;
+        if (n < 0) {
+            return 1.0/calc(x, -1 * num);
+        }
+
+        return calc(x, num);
+    }
+};
