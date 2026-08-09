d006b26be95d1520aa051e8a28946788f411cd37
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-09 11:36:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (90.15%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        string combos[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> ans;
+        string s = "";
+        func(0, digits, s, ans, combos);
+        return ans;
+    }
+
+    void func(int ind, string digits, string s, vector<string> &ans, string combos[]) {
+        if(ind == digits.size()) {
+            ans.push_back(s);
+            return;
+        }
+        int digit = digits[ind] - '0';
+        for(int i = 0; i < combos[digit].size(); i++) {
+            func(ind + 1, digits, s + combos[digit][i], ans, combos);
+        }
+    }
+};
