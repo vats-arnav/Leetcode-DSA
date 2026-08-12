ed8a2c37c3eca68c067434ea93b1246dd360b8ee
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-12 21:35:28 +0530

    Time: 14 ms (30.04%) | Memory: 17.7 MB (19.62%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -5,22 +5,16 @@ public:
 
         int n = s.length();
         vector<bool> dp(n + 1, false);
-
         dp[0] = true;
-
         for (int i = 1; i <= n; i++) {
-
             for (int j = 0; j < i; j++) {
-
                 string word = s.substr(j, i - j);
-
                 if (dp[j] && wordSet.count(word)) {
                     dp[i] = true;
                     break;
                 }
             }
         }
-
         return dp[n];
     }
 };
