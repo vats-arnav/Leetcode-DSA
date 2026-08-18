50c79892ee06577484eb9593918edb72802eaf02
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-18 12:28:09 +0530

    Time: 79 ms (40.02%) | Memory: 193.3 MB (75.09%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> findNSE(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            int currEle = arr[i];
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.empty() ? st.top() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> findPSEE(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            int currEle = arr[i];
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.empty() ? st.top() : -1;
+            st.push(i);
+        }
+
+        return ans;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        vector<int> nse = findNSE(arr);
+        vector<int> psee = findPSEE(arr);
+        int n = arr.size();
+        int mod = 1e9 + 7;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int left = i - psee[i];
+            int right = nse[i] - i;
+            long long freq = left * right * 1LL;
+            int val = (freq * arr[i] * 1LL) % mod;
+            sum = (sum + val) % mod;
+        }
+        return sum;
+    }
+};
