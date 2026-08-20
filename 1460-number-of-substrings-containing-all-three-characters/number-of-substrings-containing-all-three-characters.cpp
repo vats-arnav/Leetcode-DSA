ba8e21cd3fa1180836688f4aef6940e8ed0d8f91
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-20 11:58:27 +0530

    Time: 9 ms (47.17%) | Memory: 11 MB (78.09%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -28,7 +28,7 @@ public:
         for (int right = 0; right < s.length(); right++) {
             freq[s[right] - 'a'] = right;
             if (freq[0] != -1 && freq[1] != -1 && freq[2] != -1) {
-                res += min(freq[0], min(freq[1], freq[2])) + 1;
+                res += min({freq[0], freq[1], freq[2]}) + 1;
             }
         }
         return res;
