346057e4f04493dc450a086acac0d629b01c2a07
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-20 11:10:05 +0530

    Time: 0 ms (100.00%) | Memory: 79.6 MB (99.13%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int l = 0;
@@ -16,5 +16,37 @@ public:
             maxval = max(r - l + 1, maxval);
         }
         return maxval;
+    }
+};*/
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int left = 0, maxLen = 0;
+        int fruit1 = fruits[0], fruit2 = -1;
+        int lastFruit = fruits[0], lastPos = 0;
+
+        for (int right = 0; right < fruits.size(); right++) {
+
+            if (fruit2 == -1 && fruits[right] != fruit1) {
+                fruit2 = fruits[right];
+            }
+            else if (fruits[right] != fruit1 &&
+                     fruits[right] != fruit2) {
+
+                left = lastPos;
+                fruit1 = lastFruit;
+                fruit2 = fruits[right];
+            }
+
+            if (fruits[right] != lastFruit) {
+                lastFruit = fruits[right];
+                lastPos = right;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
     }
 };
