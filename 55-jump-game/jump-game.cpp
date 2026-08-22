52550553070a25bb26d0fc6aa7e4ccc148a36990
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-08-22 17:15:07 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (25.87%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIndex = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxIndex) {
+                return false;
+            }
+            maxIndex = max(maxIndex, nums[i] + i);
+        }
+
+        return true;
+    }
+};
