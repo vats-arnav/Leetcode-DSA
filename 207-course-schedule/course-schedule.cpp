7f44dc2bbdf3305a650c58a01c2b4033fd4e668c
Arnav Vats <77720041+vats-arnav@users.noreply.github.com>
2026-09-15 18:40:17 +0530

    Time: 7 ms (46.43%) | Memory: 19.1 MB (82.24%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,47 +1,87 @@
 class Solution {
 public:
-    bool dfsCheck(int node, vector<vector<int>>& adj, vector<int>& vis,
-                  vector<int>& pathVis) {
-
-        vis[node] = 1;
-        pathVis[node] = 1;
-
-        for (auto it : adj[node]) {
-
-            if (vis[it] == 0) {
-                if (dfsCheck(it, adj, vis, pathVis))
-                    return true;
-            } else if (pathVis[it]) {
-                return true;
-            }
-        }
-
-        pathVis[node] = 0;
-        return false;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
-        // Build adjacency list
         vector<vector<int>> adj(numCourses);
+        vector<int> inDegree(numCourses, 0);
 
-        for (auto edge : prerequisites) {
-            int course = edge[0];
-            int prerequisite = edge[1];
-
-            adj[prerequisite].push_back(course);
+        for (auto& pre : prerequisites) {
+            int a = pre[0], b = pre[1];
+            adj[b].push_back(a);
+            inDegree[a]++;
         }
 
-        vector<int> vis(numCourses, 0);
-        vector<int> pathVis(numCourses, 0);
-
+        queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (vis[i] == 0) {
-                if (dfsCheck(i, adj, vis, pathVis))
-                    return false;
+            if (inDegree[i] == 0) {
+                q.push(i);
             }
         }
 
-        return true;
+        int count = 0;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+
+            for (int nei : adj[node]) {
+                inDegree[nei]--;
+                if (inDegree[nei] == 0) {
+                    q.push(nei);
+                }
+            }
+        }
+
+        return count == numCourses;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     bool dfsCheck(int node, vector<vector<int>>& adj, vector<int>& vis,
+//                   vector<int>& pathVis) {
+
+//         vis[node] = 1;
+//         pathVis[node] = 1;
+
+//         for (auto it : adj[node]) {
+
+//             if (vis[it] == 0) {
+//                 if (dfsCheck(it, adj, vis, pathVis))
+//                     return true;
+//             } else if (pathVis[it]) {
+//                 return true;
+//             }
+//         }
+
+//         pathVis[node] = 0;
+//         return false;
+//     }
+
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+//         vector<vector<int>> adj(numCourses);
+
+//         for (auto edge : prerequisites) {
+//             int course = edge[0];
+//             int prerequisite = edge[1];
+
+//             adj[prerequisite].push_back(course);
+//         }
+
+//         vector<int> vis(numCourses, 0);
+//         vector<int> pathVis(numCourses, 0);
+
+//         for (int i = 0; i < numCourses; i++) {
+//             if (vis[i] == 0) {
+//                 if (dfsCheck(i, adj, vis, pathVis))
+//                     return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// };
